9c92304e6597fa76b55e68faf982aa662c9b46a2
Dmytro Boichuk <dboichuk@mail.greenriver.edu>
2020-05-20 16:24:14 -0400

    Step 2: Added a survey

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-
-
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -16,7 +14,21 @@ $f3 = Base::instance();
 //Default route
 $f3->route('GET /', function () {
     echo "<h1>Midterm Survey</h1><br>";
-    echo "<a href='survey'>Take my midterm survey</a>";
+    echo '<p><a href="survey">Take my midterm survey</a></p>';
+
+});
+
+$f3->route('GET /survey', function ($f3) {
+
+    $f3->set('options',array("This midterm is easy", "I like midterms",
+        "Today is Monday"));
+
+
+
+    $view = new Template();
+    echo $view->render('views/survey.html');
+
+
 });
 
 
